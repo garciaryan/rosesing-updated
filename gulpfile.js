ea0b1f4f5448251bf47b784ab6666d33d5c6c956
Ryan Garcia <garciaryan007@yahoo.com>
2018-08-13 20:41:32 -0700

    deps task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 let gulp = require('gulp'),
   del = require('del'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
   runSequence = require('run-sequence'),
-  minify = require('gulp-minify');
+  minify = require('gulp-minify'); 
 
 gulp.task('delete', () => {
   del(['dist']);
@@ -13,15 +15,27 @@ gulp.task('index', () => {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('css-deps', () => {
+  gulp.src([
+    "node_modules/aos/dist/aos.css",
+    "node_modules/animate.css/animate.min.css",
+    "node_modules/bulma/css/bulma.css"
+  ])
+    .pipe(minify())
+    .pipe(concat('deps.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('css', () => {
-  gulp.src('css/mystyles.css')
+  gulp.src([
+    'css/mystyles.css'
+  ])
     .pipe(minify())
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('js', () => {
   gulp.src('js/*.js')
-    .pipe(minify())
     .pipe(gulp.dest('dist/js'));
 });
 
@@ -30,8 +44,19 @@ gulp.task('img', () => {
     .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('js-deps', () => {
+  gulp.src([
+    "node_modules/bulma-carousel/dist/js/bulma-carousel.min.js",
+    "node_modules/aos/dist/aos.js"
+  ])
+    .pipe(uglify())
+    .pipe(minify())
+    .pipe(concat('deps.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('build', done => {
-  runSequence('delete', 'index', 'css', 'js', 'img', done);
+  runSequence('delete', 'index', 'css', 'js', 'css-deps', 'js-deps', 'img', done);
   console.log('Built!');
 });
 
